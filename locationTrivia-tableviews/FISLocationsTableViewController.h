ee27d8de18838dee954299f56ec48562e00c3577
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-04 17:15:15 -0500

    Completes exercise

---
+++ locationTrivia-tableviews/FISLocationsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISLocationsTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Ken M. Haggerty on 2/4/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISLocationsTableViewController : UITableViewController
+@property (nonatomic, strong) NSArray *locations;
+@end
